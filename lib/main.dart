212d937c286dff91660c5289a606fd343f02a1cf
Muhammed Altaf Hossain <md.altafsumon@gmail.com>
2023-01-28 22:04:49 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Todo App',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
